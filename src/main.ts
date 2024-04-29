6fa460f828873a3bce925eec064bfe15161ce53b
David-Pena <dev.davidpe@gmail.com>
2024-04-28 21:18:40 -0700

    chore: add validation to env schema

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(envs.port);
+  console.log(`Server running on port ${envs.port}`);
 }
 bootstrap();
